9765a8e784517a88931a9c96d624279782d4f253
samah <samah@DESKTOP-KFJ942F>
2020-02-08 14:15:17 +0200

    add fun

---
+++ src/Samah.java
@@ -0,0 +1,12 @@
+
+public class Samah {
+
+	public int add(int x, int y)
+	{
+		return x+y;
+	}
+	public static void main(String [] args)
+	{
+		
+	}
+}
---
+++ src/fun1.java
@@ -0,0 +1,15 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class fun1 {
+
+	@Test
+	void test() {
+		Samah ob=new Samah();
+		int act=ob.add(2,4);
+		assertTrue(6==act);
+		
+	}
+
+}
